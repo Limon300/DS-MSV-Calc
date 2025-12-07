9549f0ea8158ae52908bec0b967179397187f739
Limon300 <droidelucio@gmail.com>
2025-12-07 16:55:49 +0100

    Configuración del framework de pruebas

---
+++ CalculatorBackendSpringbootProject/src/test/java/com/calculadora/backend/CucumberTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.calculadora.backend;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("tests")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.calculadora.backend")
+public class CucumberTest {
+}
